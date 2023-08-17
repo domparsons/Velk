e4a652a5d4d2aea85c48c10876ce6cc6c3dff5e6
domparsons <dominicparsons@outlook.com>
2023-08-17 16:50:47 +0100

    Initial Commit

---
+++ Velk/VelkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelkApp.swift
+//  Velk
+//
+//  Created by Dom Parsons on 17/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct VelkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
